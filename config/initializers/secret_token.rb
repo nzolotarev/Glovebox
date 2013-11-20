231966a4bce4d568f0021e5a593981fcda21443e
Nikita Zolotarev <nzolotarev@hotmail.com>
2013-11-19 23:39:40 -0500

    Glovebox app ready, status updateis added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glovebox::Application.config.secret_token = '09a7b9f69df37d44416fb284009f33eb70ab52bde4ea64ca3f35019c19608c54f851693d7e9110ab802fb5463da3eaba8af7af14d25a8576864e022a94c02f36'
